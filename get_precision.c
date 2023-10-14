ec7ef2af43fca95285403fe5139227283800dd8b
Romisa-nor <140829716+Romisa-nor@users.noreply.github.com>
2023-10-14 18:30:02 +0300

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,38 +1,41 @@
 #include "main.h"
-#include <stdbool.h>
-
-// Function to check if a character is a digit
-bool is_digit(char c) {
-    return c >= '0' && c <= '9';
-}
 
 /**
- * get_precision - Calculates the precision for printing.
+ * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: Pointer to the current position in the format string.
- * @list: List of arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list) {
-    int curr_i = *i + 1;
-    int precision = -1;
+int get_precision(const char *format, int *i, va_list list)
+{
+	int cur_i = *i + 1;
+	int precision = -1;
 
-    // Check if the next character is a '.'
-    if (format[curr_i] != '.') {
-        return precision; // No precision specified
-    }
+	if (format[curr_i] != '.')
+		return (precision);
 
-    precision = 0;
-    curr_i++; // Move to the next character after '.'
+	precision = 0;
 
-    while (is_digit(format[curr_i])) {
-        precision *= 10;
-        precision += format[curr_i] - '0';
-        curr_i++;
-    }
+	for (cur_i += 1; format[cur_i] != '\0'; cur_i++)
+	{
+		if (is_digit(format[cur_i]))
+		{
+			precision *= 10;
+			precision += format[cur_i] - '0';
+		}
+		else if (format[cur_i] == '*')
+		{
+			crr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-    *i = curr_i - 1; // Update the position pointer
+	*i = cur_i - 1;
 
-    return precision;
+	return (precision);
 }
