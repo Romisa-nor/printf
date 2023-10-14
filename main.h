8959cd1915d66ba4cdc41faef26c2b719cd95bee
Romisa-nor <romusanor2001@gmail.com>
2023-10-14 02:00:32 -0700

    updata main.h and get_size

--- main.h
+++ main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
 #include <stdio.h>
 #include <stdarg.h>
 
@@ -8,3 +14,7 @@ int printDigit(va_list args);
 int printChar(va_list args);
 int toBinary(va_list args);
 int printUnsignDigit(va_list args);
+
+int get_size(const char *format, int *i);
+
+#endif /* MAIN_H */
